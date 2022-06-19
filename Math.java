3a9f533fa5dacc6279803d940ad46c1784b86588
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-19 07:42:46 -0700

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.lang.Math;
+class test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("enter your number;- ");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int x=num;
+		for(int k=x-1;k>=1;k--)
+			{
+				x=x+k;
+			}
+			
+			
+			System.out.println("total sum" +x);
+		
+		if(num%2==0)
+		{
+			for(int i=num-1;i>=1;i--)
+			{
+				if(i%2==0)
+				{
+					num=num+i;
+				}
+			}
+			System.out.println("sum of even "+num);
+            System.out.println("sum of odd "+(x-num));
+		}
+		else
+		{
+		
+		
+				for(int j=num-1;j>=1;j--)
+				
+					{
+						if(j%2==0)
+						{
+							num=num+j;
+		            }
+			}
+		
+			  System.out.println("sum of even "+(x-num));
+			  System.out.println("sum of odd "+num);
+	}
+}
+}
+
+	
+
+
+
+
+
+
+
+

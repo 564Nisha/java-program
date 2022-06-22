4cc7324b171e318bc6b1ab3e42bd130384559361
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-22 05:55:08 -0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] arg)
+	{
+		int a[]={10,20,30,40,50};
+	System.out.println(a[0]);
+	System.out.println(a[1]);
+	System.out.println(a[2]);
+	System.out.println(a[3]);
+	System.out.println(a[4]);
+	}
+}

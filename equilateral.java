892b1ab1a3e57f1e32e1e338441701ccd01a5ed2
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-23 12:59:22 -0700

    Add files via upload

---
+++ equilateral.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner (System.in);
+		System.out.println("enter of side: ");
+		int a=sc.nextInt();
+		double area=(1.73*a*a)/4;
+		System.out.println("area of equilateral Triangle:"+area);
+	}
+}
+		

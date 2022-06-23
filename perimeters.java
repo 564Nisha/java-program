686cb324a0828c0c8f54f8372f5f244770d6db85
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-23 13:46:48 -0700

    Add files via upload

---
+++ perimeters.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] arg)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter of side of Triangle: ");
+		int a=sc.nextInt();
+		double perimeter=3*a;
+		System.out.println("perimeter of Triangle is: "+perimeter);
+	}
+}

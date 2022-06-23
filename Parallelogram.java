176daa747873d0a0cd4601fededb8b257e2c95a9
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-23 13:06:40 -0700

    Add files via upload

---
+++ Parallelogram.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int b=sc.nextInt();
+		int h=sc.nextInt();
+		System.out.println("enter a bradth: "+b);
+		System.out.println("enter a height: "+h);
+	
+		int area_parallelogram=b*h;
+		System.out.println("area of parallelogram="+area_parallelogram);
+	}
+}

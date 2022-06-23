3e8e5d72b008be307c2d29da3d27185220d38207
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-23 13:13:29 -0700

    Add files via upload

---
+++ Trang.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] arg)
+	{
+		Scanner sc=new Scanner(System.in);
+		int b=sc.nextInt();
+		int h=sc.nextInt();
+		System.out.println("enter a breadth: "+b);
+		System.out.println("enter a hight: "+h);
+		
+		double area=(b*h)/2;
+		System.out.println("area of triangle:"+area);
+	}
+}

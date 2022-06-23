32d98c8d812ba4a53131038d8f7bb2c1763044d9
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-23 13:01:08 -0700

    Add files via upload

---
+++ Isosceles.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] arg)
+	{
+		Scanner sc=new Scanner(System.in);
+		int b=sc.nextInt();
+		int h=sc.nextInt();
+		System.out.println("enter a breadth: "+b);
+		System.out.println("enter a height: "+h);
+		double area=b*h/2;
+		System.out.println("area of Isosceles Triangle:"+area);
+	}
+}

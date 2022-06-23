1e92a52f92b23b6dcc36f455bd5e6df69583b02b
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-23 13:08:41 -0700

    Add files via upload

---
+++ perimeter.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class test
+{
+	public static void main (String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	
+	int r=sc.nextInt();
+	System.out.println("enter a radius: "+r);
+	double area=2*3.14*r;
+	System.out.println("peremeter of circle:"+area);
+	}
+}

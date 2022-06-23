f03f135d959ed15b769db17484839ac13a6aaa8a
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-23 13:11:12 -0700

    Add files via upload

---
+++ Rect.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] arg)
+	{
+		Scanner sc=new Scanner(System.in);
+		int b=sc.nextInt();
+		int h=sc.nextInt();
+		System.out.println("enter a breadth:" +b);
+		System.out.println("enter a height: " +h);
+		int area=b*h;
+		System.out.println("area of rectangale="+area);
+	}
+}

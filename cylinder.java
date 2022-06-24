d1513937570ffc3bdecbaf6f3c9d9f0aba4d1d46
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-24 08:11:19 -0700

    Add files via upload

---
+++ cylinder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class test
+{
+	
+
+ public static void main(String[] arg)S
+	{
+		
+	Scanner sc=new Scanner(System.in);
+	int h=sc.nextInt();
+	int r=sc.nextInt();
+	System.out.println("enter a height: ");
+	System.out.println("enter a radius: ");
+	double volume=3.14*r*r*h;
+	System.out.println("volume of the cylinder="+volume);
+	}
+}

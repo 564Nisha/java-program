70b4cf07836ef4b68873fe870e70f44d96ee60f8
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-24 08:06:07 -0700

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] arg)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter of side of the square: ");
+		int a=sc.nextInt();
+	
+		double perimeter=4*a;
+		System.out.println("Perimeter of square is:" +perimeter);
+	}
+}

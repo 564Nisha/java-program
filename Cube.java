fe610a6d234d7e198c358b9194876da15191455f
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-24 08:12:08 -0700

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class test
+{
+	
+
+ public static void main(String[] arg)
+	{
+		
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	System.out.println("enter side of cube: ");
+
+	double area=4*a*a;
+	System.out.println("curved surface area of cube: "+area);
+	}
+}

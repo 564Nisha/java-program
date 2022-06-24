25b9058c397a73e94286d0538a15f7a79752fe96
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-24 08:06:56 -0700

    Add files via upload

---
+++ Sphare.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class test
+{
+	
+
+ public static void main(String[] arg)
+	{
+		
+	Scanner sc=new Scanner(System.in);
+	int r=sc.nextInt();
+
+	double volume=(4/3)*3.14*r*r*r;
+	System.out.println("volume of the spharer="+volume);
+	}
+}

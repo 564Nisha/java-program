7d0333a2231b350bc5c1c50a6e506341aec6865b
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-06-24 07:26:46 -0700

    Add files via upload

---
+++ Prism.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class test
+{
+	public static void main(String[] arg)
+	{
+		
+	Scanner sc=new Scanner(System.in);
+	int b=sc.nextInt();
+	int h=sc.nextInt();
+	System.out.println("enter a area of base: ");
+	System.out.println("enter a height: ");
+	double area=b*h;
+	System.out.println("volume of prism is: "+area);
+	}
+}
+
+	

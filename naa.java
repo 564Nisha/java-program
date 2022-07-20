ec9c9d2ade0293c4450522057ed6a42614b8c887
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-07-20 06:38:40 -0700

    Add files via upload

---
+++ naa.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class test
+{
+	public static void main(String args[])
+	{
+	    float v=3.0f;
+		float u=4.0f;
+		float a=2.0f;
+		float s=3.0f;
+		float total=(v*v-u*u)/(2*a*s);
+		System.out.println(total);
+	}
+}

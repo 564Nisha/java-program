2936bde0dd54146f3aab5f39c6ca7391012d3a22
564Nisha <107789761+564Nisha@users.noreply.github.com>
2022-08-01 05:22:15 -0700

    Add files via upload

---
+++ volume cntr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.Math;
+
+class test
+{
+	public static void main (String[] args)
+	{
+
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    
+		
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		System.out.println(Math.abs(x-y));
+		}
+	}
+}
